d5aa42e3c12859ee1b6e0415389767ab48811ac4
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-12-26 14:26:01 -0500

    se corrije angular json

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { UsersComponent } from './pages/users/users.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    UsersComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
